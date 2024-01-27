d5bb666ae67ce2a9a412c079ed20953788334b53
Boudewijn Overvliet <boudewijnovervliet@BoudewiacStudio.fritz.box>
2024-01-28 00:17:02 +0100

    wip

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ use Symfony\Component\VarDumper\Dumper\ContextProvider\CliContextProvider;
 use Symfony\Component\VarDumper\Dumper\ContextProvider\SourceContextProvider;
 use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 use Symfony\Component\VarDumper\Dumper\ServerDumper;
+use Symfony\Component\VarDumper\VarDumper;
 
 $cloner = new VarCloner();
 $fallbackDumper = \in_array(\PHP_SAPI, ['cli', 'phpdbg']) ? new CliDumper() : new HtmlDumper();
@@ -16,7 +17,7 @@ $dumper = new ServerDumper('tcp://127.0.0.1:9912', $fallbackDumper, [
     'source' => new SourceContextProvider(),
 ]);
 
-\Symfony\Component\VarDumper\VarDumper::setHandler(function ($var) use ($cloner, $dumper) {
+VarDumper::setHandler(function ($var) use ($cloner, $dumper) {
     $dumper->dump($cloner->cloneVar($var));
 });
 
